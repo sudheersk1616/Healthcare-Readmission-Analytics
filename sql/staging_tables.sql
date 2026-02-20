ec3613ae14450f323a6afb57ccd5be52ee5583b1
Sudheer Sagar <86648962+sudheersk1616@users.noreply.github.com>
2026-02-20 20:57:04 +0530

    Add files via upload

---
+++ sql/staging_tables.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE healthcare_dw;
+USE healthcare_dw;
+
+CREATE TABLE stg_hospital_readmission (
+    patient_id VARCHAR(10),
+    age INT,
+    age_group VARCHAR(10),
+    gender VARCHAR(10),
+    department VARCHAR(50),
+    diagnosis VARCHAR(50),
+    length_of_stay INT,
+    num_medications INT,
+    num_procedures INT,
+    admission_type VARCHAR(20),
+    discharge_disposition VARCHAR(20),
+    readmitted VARCHAR(5)
+);
+select * from stg_hospital_readmission;
+SELECT * FROM stg_hospital_readmission LIMIT 10;
+
+
+
+
+
+
